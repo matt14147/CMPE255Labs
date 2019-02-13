9fcc95e3fa2df607090d7394e594c73a847738f1
matt14147 <matt_huang14@hotmail.com>
2019-02-12 21:45:23 -0800

    Add files via upload

---
+++ lab2/friends.py
@@ -0,0 +1,12 @@
+def mean_num_friends(x):
+    # TODO
+
+def median_num_friends(x):
+    # TODO
+
+num_friends=[100, 49, 41, 40, 25, 10, 5, 4, 7, 20, 60]
+print("mean={}".format(mean_num_friends(num_friends))
+
+print("median={}".format(median_num_friends(num_friends))
+
+
